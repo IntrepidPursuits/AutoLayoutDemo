f068714d8fa7a7be847a0d1ed49c4b89474a67e1
LuQuan Intrepid <lu@intrepid.io>
2016-05-25 22:18:35 -0400

    Initial Project

---
+++ AutoLayoutDemo/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  AutoLayoutDemo
+//
+//  Created by Intrepid-Pursuits on 5/25/16
+//  Copyright (c) 2016 Intrepid-Pursuits. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+source 'https://github.com/CocoaPods/Specs'
+
+platform :ios, '8.0'
+
+# Add Application pods here
+
+target :unit_tests, :exclusive => true do
+  link_with 'UnitTests'
+
+end
+
+
