d0081469dc96907722321fc17fd277974f70e157
LuQuan Intrepid <lu@intrepid.io>
2016-05-27 15:55:19 -0400

    Add ALMenuLabelView

---
+++ AutoLayoutDemo/Views/Menu/ALMenuLabelView.swift
@@ -0,0 +1,20 @@
+//
+//  ALMenuLabelView.swift
+//  AutoLayoutDemo
+//
+//  Created by LuQuan Intrepid on 5/27/16.
+//  Copyright © 2016 Intrepid-Pursuits. All rights reserved.
+//
+
+import UIKit
+
+class ALMenuLabelView: UIView {
+
+    @IBOutlet var label: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
