a8bd302a5614b17d40d394519abc13eef2bdf69f
LuQuan Intrepid <lu@intrepid.io>
2016-06-01 10:49:55 -0400

    Set up MenuViewController xib without constraints on the views
    set outlets on menuVC

--- AutoLayoutDemo/Controllers/Menu/ALMenuViewController.swift
+++ AutoLayoutDemo/Controllers/Menu/ALMenuViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ALMenuViewController: UIViewController {
 
     @IBOutlet var menuContainerView: UIView!
+    @IBOutlet var view1: UIView!
+    @IBOutlet var view2: UIView!
+    @IBOutlet var view3: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,4 +22,5 @@ class ALMenuViewController: UIViewController {
     @IBAction func dismissButtonPressed(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
     }
+
 }
