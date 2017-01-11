78afc5b609b90288c695efeba430f7d8d6ccc162
LuQuan Intrepid <lu@intrepid.io>
2017-01-11 17:32:03 -0500

    New Project

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by LuQuan Intrepid on 1/11/17.
+//  Copyright © 2017 IntrepidPursuits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
